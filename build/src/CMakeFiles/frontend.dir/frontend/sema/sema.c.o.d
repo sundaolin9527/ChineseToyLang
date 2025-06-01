2972717ac30a45cdbbd3b20762bca0814f2d9ae5
sundaolin <sundaolin 2908879551@qq.com>
2025-06-01 17:38:52 +0800

    删除build目录

--- build/src/CMakeFiles/frontend.dir/frontend/sema/sema.c.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/frontend.dir/frontend/sema/sema.c.o: \
- /opt/elang/src/frontend/sema/sema.c /usr/include/stdc-predef.h
--- build/src/version.h
+++
@@ -1,6 +0,0 @@
-#ifndef ELANG_VERSION_H
-#define ELANG_VERSION_H
-
-#define ELANG_VERSION "1.0.0"
-
-#endif
